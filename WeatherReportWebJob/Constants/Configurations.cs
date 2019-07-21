8e4fcea09ee0f9a487cba25ebac7ba07d0483b64
Nitin Saggam (Infosys Ltd) <v-nisagg@microsoft.com>
2019-07-21 20:54:50 +0530

    imput and outputLocation for file retrieval and storage is added

--- WeatherReportWebJob/Constants/Configurations.cs
+++ WeatherReportWebJob/Constants/Configurations.cs
@@ -5,10 +5,10 @@
         public const string ApiKey = "aa69195559bd4f88d79f9aadeb77a8f6";
 
         // cityIds file path location
-        public const string FilePath = @"filePath\CityDetailsSheet.csv";
+        public const string FilePath = @"~\inputLocation\CityDetailsSheet.csv";
         public const string OpenWeatherMapApi = "http://api.openweathermap.org/data/2.5/weather?id=cityId&appid=" + ApiKey + "&units=metric";
 
         // weather report storage location of cities
-        public const string CityReportLocation = @"storagePath\CityWeatherReport\";
+        public const string CityReportLocation = @"~\outputLocation\";
     }
 }
